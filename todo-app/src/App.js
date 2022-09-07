915c6d09c33a466cbbebd44ea5e2c7bdbc1a3e95
Naomi-Vega <97486808+Naomi-Vega@users.noreply.github.com>
2022-09-07 19:18:11 -0400

    First test list

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./App.css";
+import TodoList from "./Components/todoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To-do list</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
